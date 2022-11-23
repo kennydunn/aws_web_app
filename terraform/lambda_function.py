b9fe531652aa4d479597be7fc9aba9a0cca9d184
kennyd <kennydunn@live.co.uk>
2022-11-23 21:55:43 +0000

    terraform code

--- terraform/lambda_function.py
+++ terraform/lambda_function.py
@@ -16,10 +16,18 @@ now = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
 def lambda_handler(event, context):
 # extract values from the event object we got from the Lambda service and store in a variable
     name = event['firstName'] +' '+ event['lastName']
+    firstName = event['firstName']
+    lastName = event['lastName']
+    age = event['age']
+    city = event['city']
 # write name and time to the DynamoDB table using the object we instantiated and save response in a variable
     response = table.put_item(
         Item={
             'ID': name,
+            'FirstName': firstName,
+            'LastName': lastName,
+            'Age': age,
+            'City': city,
             'LatestGreetingTime':now
             })
 # return a properly formatted JSON object
